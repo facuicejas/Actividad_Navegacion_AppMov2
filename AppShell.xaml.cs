ce73b01c92f1a9fc1afc5f4b4f3979db50a602bc
facuicejas <facundo.i.cejas@gmail.com>
2026-05-31 13:52:55 -0300

    Archivos Base del proyecto

---
+++ AppShell.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Parcial1Cejas.Views;
+
+namespace Parcial1Cejas
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Registro de todas las rutas
+            Routing.RegisterRoute(
+                nameof(DetailPage),
+                typeof(DetailPage));
+
+            Routing.RegisterRoute(
+                nameof(AddGamePage),
+                typeof(AddGamePage));
+        }
+    }
+}
---
+++ MauiProgram.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Parcial1Cejas.Services;
+using Parcial1Cejas.ViewModels;
+using Parcial1Cejas.Views;
+
+namespace Parcial1Cejas
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont(
+                        "OpenSans-Regular.ttf",
+                        "OpenSansRegular");
+
+                    fonts.AddFont(
+                        "OpenSans-Semibold.ttf",
+                        "OpenSansSemibold");
+                });
+
+            // Estos son los servicios que va a usar el programa
+            builder.Services.AddSingleton<ApiService>();
+            builder.Services.AddSingleton<NavigationService>();
+
+            // Estos son los ViewModels del programa
+            builder.Services.AddTransient<MainViewModel>();
+            builder.Services.AddTransient<DetailViewModel>();
+            builder.Services.AddTransient<AddGameViewModel>();
+
+            // Estas son las Views del programa
+            builder.Services.AddTransient<MainPage>();
+            builder.Services.AddTransient<DetailPage>();
+            builder.Services.AddTransient<AddGamePage>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
